ec551f4b0789ab6ffc1f3826bab197f600a2ffa3
amitjha25in <amit.jha@candere.com>
2025-08-01 12:44:03 +0530

    update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Heart, Layers } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge"
 
 const ProductCard = ({item}) => {
     console.log(item);
   return (
-    <div className="product-card border-1 rounded-2xl bg-white border-gray-300 overflow-hidden group transition-shadow duration-200 ease-in-out hover:shadow-lg ">
-      <div className="cardHeader bg-gray-100 relative group">
+    <div className="product-card rounded-2xl bg-white border-gray-300 overflow-hidden group transition-shadow duration-200 ease-in-out hover:shadow-lg ">
+      <div className="cardHeader bg-gray-100 relative group border-1 rounded-2xl overflow-hidden">
         {item.product_data?.images && <img src={item.product_data?.images[0]?.metal_image} alt={item.product_name} className="transition-opacity duration-300 ease-in-out mix-blend-darken group-hover:opacity-0 cursor-pointer w-full h-full object-cover"/>}        
-        {item.product_data?.images[5] ? <img src={item.product_data?.images[5]?.metal_image} alt={item.product_name} className="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 mix-blend-darken cursor-pointer"/> : <img src={item.product_data?.images[4]?.metal_image} alt={item.product_name} className="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 mix-blend-darken cursor-pointer"/> }        
+        {item.product_data?.images[5] ? <img src={item.product_data?.images[5]?.metal_image} alt={item.product_name} className="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 mix-blend-darken cursor-pointer"/> : <img src={item.product_data?.images[4]?.metal_image} alt={item.product_name} className="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 mix-blend-darken cursor-pointer"/> } 
+        <Button variant="none" size="icon" className="size-8 absolute top-1 right-1 cursor-pointer">
+           <Heart/>     
+        </Button>
+        <div className="absolute top-1 left-1">
+          <Badge variant="secondary" className="text-[12px] uppercase">{item.label}</Badge>
+        </div>
+        <Button variant="none" size="icon" className="size-8 absolute bottom-1 right-1 cursor-pointer">
+           <Layers size={16}/>    
+        </Button>
+        <span>
+          
+        </span>
       </div>
-      <div className="cardContent bg-white text-center pb-4 pt-2">
-            <div className="flex items-center gap-2 line-clamp-1 justify-center px-4">
+      <div className="cardContent bg-white pb-4 pt-2">
+            <div className="flex items-center gap-2 line-clamp-1 justify-start px-4">
                 <div className="text-cyan-800 text-md">₹{parseInt(Math.abs(item.offer_price))}</div>
                 <div className="text-gray-500 text-sm line-through">₹{parseInt(Math.abs(item.price))}</div>
             </div>
-            <div className="text-[12px] text-white my-1 line-clamp-1 bg-cyan-700 px-4">{item.offer_text}</div>
+            <div className="text-[12px] text-cyan-700 my-1 line-clamp-1 px-4">{item.offer_text}</div>
             <div className="text-[12px] text-gray-500 line-clamp-1 px-4">{item.product_name}</div>
       </div>
     </div>
--- src/pages/Jewellery.jsx
+++ src/pages/Jewellery.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router'
-import { SlashIcon, X } from "lucide-react"
+import { SlashIcon, X, Settings2, ArrowDownUp, ChartNoAxesGantt } from "lucide-react"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -19,90 +19,198 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { Badge } from "@/components/ui/badge"
 import InitialFilter from "@/api/initialFilter";
-import products from "@/api/product"
+import productData from "@/api/productData"
 import ProductCard from '@/components/ProductCard';
+import StickyBox from "react-sticky-box";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import {Pagination } from 'swiper/modules';
+import useWindowSize from '@/hooks/useWindowSize';
+//drawer
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
+const FilterBottomSheet = ({ open, setOpen, type }) => {
+  const getTitle = () => {
+    if (type === 'categories') return 'Categories';
+    if (type === 'sort') return 'Sort';
+    if (type === 'filter') return 'Filter';
+    return 'Drawer';
+  };
+
+  const getDescription = () => {
+    if (type === 'categories') return 'Choose a category';
+    if (type === 'sort') return 'Select a sort order';
+    if (type === 'filter') return 'Set your filters';
+    return '';
+  };
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>{getTitle()}</DrawerTitle>
+            <DrawerDescription>{getDescription()}</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">test</div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button variant="outline" onClick={() => setOpen(false)}>Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 
 const Jewellery = () => {
+  const [open, setOpen] = useState(false);
+  const [activeType, setActiveType] = useState(null);
+  const { width } = useWindowSize();
   const [selected, setSelected] = useState("all");
 
   const setSelectedCategory = (label) => {
     setSelected(label);
   }
+
   return (
     <div className="section-list">
-      <div className="section-breadcrumb bg-green-50 py-4 px-5 lg:px-20">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link to="/">Home</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator>
-              <SlashIcon />
-            </BreadcrumbSeparator>         
-            <BreadcrumbItem>
-              <BreadcrumbPage>All Jewellery</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-      <div className="flex justify-start flex-wrap items-center gap-2 md:flex-row px-5 lg:px-20 my-10">
-        {
-          InitialFilter.map((item) => (
-            <Button
-              key={item.label}
-              onClick={() => setSelectedCategory(item.label)}
-              variant={selected === item.label ? "default" : "secondary"}
-              className="cursor-pointer"
-            >
-              {item.name}
-            </Button>
-          ))
-        }
-      </div>
-      <div className="product-list-section block lg:grid lg:grid-cols-[25%_75%] px-5 lg:px-20">
-        <div className="filter-sidebar block">1</div>
-        <div className="product-list block">
-          <div className="selected-filter w-full flex flex-col lg:flex-row lg:justify-between items-start lg:items-center gap-4">
-            <div className="flex flex-wrap gap-2">             
-              <Button variant="outline" size="sm" className="py-2 px-4 rounded-full cursor-pointer">
-                Rings <X className="cursor-pointer" size={16}/>
-              </Button>
-              <Button variant="outline" size="sm" className="py-2 px-4 rounded-full cursor-pointer">
-                Rs.10001 To Rs.15000 <X className="cursor-pointer" size={16}/>
-              </Button>
-            </div>          
-            <div className="sorting-filter">
-              <Select>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Latest" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="discount">Discount</SelectItem>
-                    <SelectItem value="featured">Featured</SelectItem>
-                    <SelectItem value="lowtohigh">Price:Low to High</SelectItem>
-                    <SelectItem value="hightolow">Price:High to Low</SelectItem>
-                    <SelectItem value="customerrating">Customer Rating</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
+      {
+        width >= 1023 &&      
+        <>
+          <div className="section-breadcrumb bg-green-50 py-4 px-5 lg:px-20">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link to="/">Home</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                  <SlashIcon />
+                </BreadcrumbSeparator>         
+                <BreadcrumbItem>
+                  <BreadcrumbPage>All Jewellery</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-          <div className="product-list-container mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+          <div className="flex justify-start flex-wrap items-center gap-2 md:flex-row px-5 lg:px-20 my-10">
             {
-              products.map((item)=>(
+              InitialFilter.map((item) => (
+                <Button
+                  key={item.label}
+                  onClick={() => setSelectedCategory(item.label)}
+                  variant={selected === item.label ? "default" : "secondary"}
+                  className="cursor-pointer"
+                >
+                  {item.name}
+                </Button>
+              ))
+            }
+          </div>
+        </>
+     }
+      {width <= 1023 &&  
+        <div className="filter-list my-4">
+            <Swiper
+               slidesPerView={'auto'}
+               spaceBetween={15}
+               modules={[Pagination]}
+            >
+                  {
+                    InitialFilter.map((item) => (
+                      <SwiperSlide key={item.label}>
+                            <Button                              
+                              onClick={() => setSelectedCategory(item.label)}
+                              variant={selected === item.label ? "default" : "secondary"}
+                              className="cursor-pointer"
+                            >
+                              {item.name}
+                            </Button>
+                      </SwiperSlide>
+                    ))
+                  }
+            </Swiper>
+        </div>        
+      }
+
+      <div className="product-list-section block lg:grid lg:grid-cols-[25%_75%] px-5 lg:px-20">
+          <div className="filter-sidebar block">
+            <StickyBox offsetTop={20} offsetBottom={20}>test</StickyBox>
+          </div>
+        <div className="product-list block">
+          {
+            width > 1023 &&          
+            <div className="selected-filter w-full flex flex-col lg:flex-row lg:justify-between items-start lg:items-center gap-4 mb-6">
+              <div className="flex flex-wrap gap-2">             
+                <Button variant="outline" size="sm" className="py-2 px-4 rounded-full cursor-pointer">
+                  Rings <X className="cursor-pointer" size={16}/>
+                </Button>
+                <Button variant="outline" size="sm" className="py-2 px-4 rounded-full cursor-pointer">
+                  Rs.10001 To Rs.15000 <X className="cursor-pointer" size={16}/>
+                </Button>
+              </div>          
+              <div className="sorting-filter">
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Latest" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="discount">Discount</SelectItem>
+                      <SelectItem value="featured">Featured</SelectItem>
+                      <SelectItem value="lowtohigh">Price:Low to High</SelectItem>
+                      <SelectItem value="hightolow">Price:High to Low</SelectItem>
+                      <SelectItem value="customerrating">Customer Rating</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          }
+          <div className="product-list-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+            {
+              productData.map((item)=>(
                 <ProductCard item={item} key={item.product_id}/>  
               ))
             }
           </div>
         </div>
       </div>
+        {width <= 1023 && 
+          <div className="fixed bottom-0 left-0 w-full bg-black flex items-center justify-between text-white px-2">
+            <div className="h-14 flex justify-center items-center text-white">
+              <Button variant="none" onClick={() => { setActiveType('categories'); setOpen(true); }}>
+                <ChartNoAxesGantt /> <span className="text-[12px] uppercase">Categories</span>
+              </Button>
+            </div>
+            <div className="h-14 flex justify-center items-center text-white">
+              <Button variant="none" onClick={() => { setActiveType('sort'); setOpen(true); }}>
+                <ArrowDownUp /> <span className="text-[12px] uppercase">Sort</span>
+              </Button>
+            </div>
+            <div className="h-14 flex justify-center items-center text-white">
+              <Button variant="none" onClick={() => { setActiveType('filter'); setOpen(true); }}>
+                <Settings2 /> <span className="text-[12px] uppercase">Filter</span>
+              </Button>
+            </div>
+          </div>
+        }
+       <FilterBottomSheet open={open} setOpen={setOpen} type={activeType} />
     </div>
   )
 }
